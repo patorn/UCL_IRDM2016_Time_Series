2d3350a29674b9a4d8ef229a5d68da06a2886942
Patorn Utenpattanun <patorn.u@gmail.com>
2016-04-07 09:58:18 +0100

    Fix MAPE

--- python/metrics.py
+++ python/metrics.py
@@ -3,7 +3,7 @@ import numpy as  np
 from sklearn import metrics
 
 def mape(predictions, true_labels):
-  return np.mean(np.abs(predictions - true_labels) / true_labels)
+  return np.mean(np.abs((true_labels - predictions) / true_labels)) * 100
 
 def rmse(predictions, true_labels):
   return metrics.mean_squared_error(predictions, true_labels) ** 0.5
