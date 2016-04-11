a988eb7bc7d0c274bedc406c9cc56df007276346
MUHAMMAD FAIZ MOHD ZAKI <muhammadfaiz.zaki@gmail.com>
2016-04-12 00:04:57 +0100

    mean to sum correction

--- R/Utils.R
+++ R/Utils.R
@@ -1,6 +1,6 @@
 #function to aggregate by day
 agg_by_day <- function(originaldata) {
-  meanByDay <- function(x) c(mean = mean(x))
+  meanByDay <- function(x) c(sum = sum(x))
   originaldata$Date <- as.Date(originaldata$Date, "%d/%m/%Y")
   byDay <- aggregate(Global_active_power ~ Date, originaldata, meanByDay)
   return(byDay)
@@ -9,7 +9,7 @@ agg_by_day <- function(originaldata) {
 #function to aggregate by month
 agg_by_month <- function(originaldata){
   
-  agg_Month <- aggregate(originaldata,by=list(as.yearmon(tmp_data$Date,"%d/%m/%Y")), FUN=mean, na.rm=TRUE)
+  agg_Month <- aggregate(originaldata,by=list(as.yearmon(tmp_data$Date,"%d/%m/%Y")), FUN=sum, na.rm=TRUE)
   byMonth <- agg_Month[,-2]
   byMonth <- byMonth[,-2]
   byMonth <- rename(byMonth, c("Group.1"="Month"))
