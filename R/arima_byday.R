86e81ff5e2370a740da7908607ccf55dd586e1eb
MUHAMMAD FAIZ MOHD ZAKI <muhammadfaiz.zaki@gmail.com>
2016-04-12 11:57:39 +0100

    updated codes for daily and monthly

--- R/arima_byday.R
+++ R/arima_byday.R
@@ -3,25 +3,17 @@ source("Utils.R")
 library(xts, zoo, forecast)
 
 #read dataset
-data<-read.table("household_power_consumption.txt", header=T, sep=";")
-
-#only use date, time and global_active_power
-tmp_data<-data[,1:3] 
+tmp_data<-read.table("household_power_consumption_daily.csv", header=T, sep=";")
 
 #replace nans
 NAs <- tmp_data == "?"
 is.na(tmp_data)[NAs] <- TRUE
 tmp_data$Global_active_power <- na.locf(tmp_data$Global_active_power, fromLast = FALSE) 
 
-#convert column to numeric
-tmp_data$Global_active_power <- as.numeric(as.character(tmp_data$Global_active_power))
-
-#aggregate data by day
-data_byDay <- agg_by_day(tmp_data)
 
 #create time series object
-tsDay_test <- ts(data_byDay[1:49,2],frequency=58, start=c(2006,12), end=c(2007,2))
-tsDay_actual <- ts(data_byDay[50:51,2],frequency=7, start=c(2007,2))
+tsDay_test <- ts(tmp_data[1:49,2],frequency=58, start=c(2006,12), end=c(2007,2))
+tsDay_actual <- ts(tmp_data[50:51,2],frequency=7, start=c(2007,2))
 
 #forecast and plot
 fit <- auto.arima(tsDay_test)
@@ -37,8 +29,8 @@ actualValues <- data.frame(data_byDay[c(1:50),])
 
 forecastValues <- fitted(fit)
 forecastValues <- data.frame(forecastValues)
-forecastValues <- rename(forecastValues, c("forecastValues"="Forecasted Value"))
-forecastValues$"Forecasted Value" <- as.numeric(forecastValues$"Forecasted Value")
+forecastValues <- rename(forecastValues, c("forecastValues"="Forecasted_Value"))
+forecastValues$Forecasted_Value <- as.numeric(forecastValues$Forecasted_Value)
 forecastValues[nrow(forecastValues) + 1,]<-c(pred$mean[1])
 
 
--- R/arima_bymonth.R
+++ R/arima_bymonth.R
@@ -1,26 +1,18 @@
 
 source("Utils.R")
 
-#read dataset
-data<-read.table("household_power_consumption.txt", header=T, sep=";")
-
-#only use date, time and global_active_power
-tmp_data<-data[,1:3] 
+#read aggregated dataset (monthly)
+tmp_data<-read.table("household_power_consumption_monthly.csv", header=T, sep=";")
 
 #replace nans
 NAs <- tmp_data == "?"
 is.na(tmp_data)[NAs] <- TRUE
 tmp_data$Global_active_power <- na.locf(tmp_data$Global_active_power, fromLast = FALSE) 
 
-#convert column to numeric
-tmp_data$Global_active_power <- as.numeric(as.character(tmp_data$Global_active_power))
-
-#aggregate data by month
-data_byMonth <- agg_by_month(tmp_data)
 
 #create time series object
-tsMonth_test <- ts(data_byMonth[1:42,2],frequency=12, start=c(2006,12))
-tsMonth_actual <- ts(data_byMonth[43:48,2],frequency=12, start=c(2010,6))
+tsMonth_test <- ts(tmp_data[1:42,2],frequency=12, start=c(2006,12))
+tsMonth_actual <- ts(tmp_data[43:48,2],frequency=12, start=c(2010,6))
 
 #forecast and plot
 fit <- auto.arima(tsMonth_test)
@@ -33,12 +25,12 @@ lines(tsMonth_actual, col="green")
 eval <- accuracy(pred, tsMonth_actual)
 
 #copy actual and forecasted readings into data frame
-actualValues <- data.frame(data_byMonth[,])
+actualValues <- tmp_data[1:48,]
 
 forecastValues <- fitted(fit)
 forecastValues <- data.frame(forecastValues)
-forecastValues <- rename(forecastValues, c("forecastValues"="Forecasted Value"))
-forecastValues$"Forecasted Value" <- as.numeric(forecastValues$"Forecasted Value")
+forecastValues <- rename(forecastValues, c("forecastValues"="Forecasted_Value"))
+forecastValues$Forecasted_Value <- as.numeric(forecastValues$Forecasted_Value)
 
 #copy each forecast
 forecastValues[nrow(forecastValues) + 1,]<-c(pred$mean[1])
