621c9e1a39063c310e1fd930626ba1c4a7e2ed94
Patorn Utenpattanun <patorn.u@gmail.com>
2016-04-14 00:08:13 +0100

    Add legend to graph

--- python/graph_utils.py
+++ python/graph_utils.py
@@ -5,8 +5,11 @@ def plot(graph_name, predictions, true_labels):
   try:
     fig = plt.figure()
     ax = fig.add_subplot(111)
-    ax.plot(true_labels[:100]) # np.concatenate((y_train, y_test[:100]))
-    plt.plot(predictions[:100]) # np.concatenate((X_train, predictions[:100]))
+    ax.plot(true_labels[:100], label='Original Series') # np.concatenate((y_train, y_test[:100]))
+    plt.plot(predictions[:100], label='Predictions') # np.concatenate((X_train, predictions[:100]))
+    plt.legend(loc='upper right')
+    plt.xlabel('Timestep')
+    plt.ylabel('Global Active Power')
     plt.savefig('../output/' + graph_name)
     plt.close()
   except Exception as e:
